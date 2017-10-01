bd68e8bb757ff9a3e256ab2be1484036d7c2ce12
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2017-10-01 19:24:31 -0400

    initial implementation of DFS

--- 1/searchdir/blindSearch/depthfirst_search.py
+++ 1/searchdir/blindSearch/depthfirst_search.py
@@ -5,3 +5,29 @@ from searchdir.util import *
 ## It must return the solution node and the number of visited nodes
 def depthfirst_search(initialState):
     print('DFS ----------------------------------')
+    root = Node(initialState)
+
+    return depthfirst_search_rec(root)
+
+def depthfirst_search_rec(node, visitedCtr=0):
+    visitedCtr += 1
+
+    if node.state.isGoal():
+        return node, visitedCtr
+
+    if node.isRepeated():
+        return None, visitedCtr
+
+    frontier = Stack()
+    for expanded in node.expand():
+        frontier.push(expanded)
+
+    while not frontier.isEmpty():
+        expanded = frontier.pop()
+        solution, subCtr = depthfirst_search_rec(expanded, visitedCtr)
+        visitedCtr += subCtr
+
+        if solution:
+            return expanded, visitedCtr
+
+    return None, visitedCtr
