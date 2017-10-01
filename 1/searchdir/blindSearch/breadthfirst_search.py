49a4ba7fc9c907d4c478a9c3cfe5bd76ee4aad8d
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2017-10-01 19:47:04 -0400

    initial implementation of BFS

--- 1/searchdir/blindSearch/breadthfirst_search.py
+++ 1/searchdir/blindSearch/breadthfirst_search.py
@@ -5,7 +5,26 @@ from searchdir.util import *
 ## It must return the solution node and the number of visited nodes
 def breadthfirst_search(initialState):
     print('BFS------------------------------')
+    root = Node(initialState)
+    toVisit = Queue()
+    toVisit.enqueue(root)
 
+    return breadthfirst_search_rec(toVisit)
 
+def breadthfirst_search_rec(toVisit, visitedCtr=0):
+    if toVisit.isEmpty():
+        return None, visitedCtr
 
+    node = toVisit.dequeue()
+    visitedCtr += 1
 
+    if node.state.isGoal():
+        return node, visitedCtr
+
+    if node.isRepeated():
+        return breadthfirst_search_rec(toVisit, visitedCtr)
+
+    for expanded in node.expand():
+        toVisit.enqueue(expanded)
+
+    return breadthfirst_search_rec(toVisit, visitedCtr)
