a2cecbee00f7e922b525f557da0653d5e42cbbea
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2017-10-02 22:01:48 -0400

    put __init__.py everywhere

--- 1/problems/eightPuzzle/EightPuzzleState.py
+++ 1/problems/eightPuzzle/EightPuzzleState.py
@@ -82,13 +82,13 @@ class EightPuzzleState(State):
     ## returns the value of your first heuristic for the current state
     # make sure to explain it clearly in your comment
     def heuristic1(self):
-        # TO COMPLETE
+        pass# TO COMPLETE
 
 
     # returns the value of your first heuristic for the current state
     # make sure to explain it clearly in your comment
     def heuristic2(self, matrix, goal):
-        # TO COMPLETE
+        pass# TO COMPLETE
 
 EightPuzzleState.goal = [0,1,2,3,4,5,6,7,8]
 
@@ -154,9 +154,9 @@ else:
     stop = timeit.default_timer()
     printResults('DFS', solution, start, stop, nbvisited)
 
-    start = timeit.default_timer()
-    solution, nbvisited = astar_search(puzzle)
-    stop = timeit.default_timer()
-    printResults('A*', solution, start, stop, nbvisited)
+   # start = timeit.default_timer()
+    #solution, nbvisited = astar_search(puzzle)
+    #stop = timeit.default_timer()
+    #printResults('A*', solution, start, stop, nbvisited)
 
 
