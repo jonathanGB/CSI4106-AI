d54cdc50f59d99eba4d981fc3e104896f908c673
Alejandro Rodriguez Salamanca <alexrs95@gmail.com>
2017-10-02 22:54:40 -0400

    Move code from EighPuzzleState.py to blind_search.py for testing

---
+++ 1/blind_search.py
@@ -0,0 +1,37 @@
+from problems.eightPuzzle.EightPuzzleState import EightPuzzleState
+import timeit
+
+
+#######  SEARCH ###########################
+EIGHT_PUZZLE_DATA = [[0, 1, 2, 3, 4, 5, 6, 7, 8],
+                     [1, 0, 2, 3, 4, 5, 6, 7, 8],
+                     [1, 0, 2, 3, 4, 5, 8, 7, 6],
+                     [4, 0, 6, 1, 2, 8, 7, 3, 5],
+                     [1, 2, 8, 7, 3, 4, 5, 6, 0],
+                     [5, 1, 3, 4, 0, 2, 7, 6, 8],
+                     [1, 2, 5, 7, 6, 8, 0, 4, 3],
+                     [4, 6, 0, 7, 2, 8, 3, 1, 5]]
+
+puzzle_choice = EIGHT_PUZZLE_DATA[6]
+puzzle = EightPuzzleState(puzzle_choice)
+#puzzle, puzzle_choice = randomize(puzzle)
+print('Initial Config')
+puzzle.show()
+if not issolvable(puzzle_choice):
+    print("NOT SOLVABLE")
+else:
+    start = timeit.default_timer()
+    solution, nbvisited = breadthfirst_search(puzzle)
+    stop = timeit.default_timer()
+    printResults('BFS', solution, start, stop, nbvisited)
+
+
+    start = timeit.default_timer()
+    solution, nbvisited = depthfirst_search(puzzle)
+    stop = timeit.default_timer()
+    printResults('DFS', solution, start, stop, nbvisited)
+
+   # start = timeit.default_timer()
+    #solution, nbvisited = astar_search(puzzle)
+    #stop = timeit.default_timer()
+    #printResults('A*', solution, start, stop, nbvisited)
--- 1/problems/eightPuzzle/EightPuzzleState.py
+++ 1/problems/eightPuzzle/EightPuzzleState.py
@@ -3,8 +3,6 @@
 ## Author: Hadi Abdi Ghavidel
 ## habdi.cnlp@gmail.com
 
-import timeit
-
 import numpy as np
 import random
 from searchdir.blindSearch.breadthfirst_search import *
@@ -124,39 +122,4 @@ def randomize(puzzle):
             newState.executeAction(random.choice(list(board.possibleActions())))
             moves= moves+1
             return self.shuffle_ran(newState, moves)
-
-#######  SEARCH ###########################
-EIGHT_PUZZLE_DATA = [[0, 1, 2, 3, 4, 5, 6, 7, 8],
-                     [1, 0, 2, 3, 4, 5, 6, 7, 8],
-                     [1, 0, 2, 3, 4, 5, 8, 7, 6],
-                     [4, 0, 6, 1, 2, 8, 7, 3, 5],
-                     [1, 2, 8, 7, 3, 4, 5, 6, 0],
-                     [5, 1, 3, 4, 0, 2, 7, 6, 8],
-                     [1, 2, 5, 7, 6, 8, 0, 4, 3],
-                     [4, 6, 0, 7, 2, 8, 3, 1, 5]]
-
-puzzle_choice = EIGHT_PUZZLE_DATA[6]
-puzzle = EightPuzzleState(puzzle_choice)
-#puzzle, puzzle_choice = randomize(puzzle)
-print('Initial Config')
-puzzle.show()
-if not issolvable(puzzle_choice):
-    print("NOT SOLVABLE")
-else:
-    start = timeit.default_timer()
-    solution, nbvisited = breadthfirst_search(puzzle)
-    stop = timeit.default_timer()
-    printResults('BFS', solution, start, stop, nbvisited)
-
-
-    start = timeit.default_timer()
-    solution, nbvisited = depthfirst_search(puzzle)
-    stop = timeit.default_timer()
-    printResults('DFS', solution, start, stop, nbvisited)
-
-   # start = timeit.default_timer()
-    #solution, nbvisited = astar_search(puzzle)
-    #stop = timeit.default_timer()
-    #printResults('A*', solution, start, stop, nbvisited)
-
 
